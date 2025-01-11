d4e15b4621247e43574b5624cc63b1777497c78d
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-01-11 13:08:26 +0530

    feat : "/stats" route configuration

--- routes/coins.js
+++ routes/coins.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-const {statsCheckCoin,statsCheckCoinAll} = require("../controllers/statsController")
+const {statsCheck} = require("../controllers/statsController")
 const {deviationCheckCoin} = require("../controllers/deviationController")
 
 /* GET coins listing. */
-router.get('/stats', statsCheckCoinAll);
-
-router.get('/stats/:coin',statsCheckCoin);
+router.get('/stats', statsCheck);
 
 router.get('/deviation/:coin',deviationCheckCoin);
 
